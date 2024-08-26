f8f658222b6ffd0af5cf6b0756145160bf6a76cb
Gustavo Coronel <gcoronelc@gmail.com>
2024-08-26 12:45:21 -0500

    Semana 1

---
+++ Semana01-26AGO/Demo01/src/main/java/pe/edu/uni/demo01/service/MateService.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.demo01.service;
+
+public class MateService {
+	
+	public int sumar(int n1, int n2){
+		return (n1 + n2);
+	}
+	
+	public int multiplicar(int n1, int n2){
+		return (n1 * n2);
+	}
+    
+}
